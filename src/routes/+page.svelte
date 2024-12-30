ef80a91a7ea45e699723863e9b906fcd5ecd0e12
azez <t.s.connect.m@gmail.com>
2024-12-30 11:54:49 +0300

    🧹 chore: inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+</script>
+<section class="flex flex-col gap-4 items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold">Hello world!</h1>
+    <Button href="/playground">Playground</Button>
+</section>
